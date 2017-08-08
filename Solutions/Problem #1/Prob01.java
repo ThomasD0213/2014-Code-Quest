9e423b631272a6d81c46a0ffc54b07d9b2f69865
Aox2120 <bestmcpvp2120@yahoo.com>
2017-08-08 15:59:50 -0600

    Fixed prob01.java

--- Solutions/Problem #1/Prob01.java
+++ Solutions/Problem #1/Prob01.java
@@ -1,9 +1,19 @@
-import java.*;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Prob01
 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(new File("Prob01.in.txt"));
-        scan.nextLine();
+        try
+        {
+            Scanner scan = new Scanner(new File("Prob01.in.txt"));
+            while( scan.hasNext() )
+                System.out.println(scan.next());
+        }
+        catch( FileNotFoundException e )
+        {
+            e.printStackTrace();
+        }
     }
 }
