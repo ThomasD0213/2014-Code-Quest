450d41ca79788f6a637aea27453cbc948cd9b3af
Aox2120 <bestmcpvp2120@yahoo.com>
2017-08-08 15:51:01 -0600

    Added Problems and Started Prob01

---
+++ Solutions/Problem #1/Prob01.java
@@ -0,0 +1,9 @@
+import java.*;
+
+public class Prob01
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(new File("Prob01.in.txt"));
+        scan.nextLine();
+    }
+}
